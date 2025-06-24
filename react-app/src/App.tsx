0cb1bd318ff6e106c72a4664029a20faa4657e10
https://github.com/thyagarajsalome/KQ-V1.0.0.git <thyagarajsalome@gmail.com>
2025-06-24 12:21:33 +0530

    Button component updated

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,12 @@
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
   return (
     <div>
-      <Button onClick={() => console.log("Clicked")}>My Button</Button>
+      <Button color="primary" onClick={() => console.log("Clicked")}>
+        My Button
+      </Button>
     </div>
   );
 }
