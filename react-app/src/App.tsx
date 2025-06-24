8130ab33a281b2b231b11b76690d6fb4bc4a60f7
https://github.com/thyagarajsalome/KQ-V1.0.0.git <thyagarajsalome@gmail.com>
2025-06-24 12:17:01 +0530

    Button added

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,9 @@
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   return (
-    <div className="alert alert-primary">
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+    <div>
+      <Button onClick={() => console.log("Clicked")}>My Button</Button>
     </div>
   );
 }
---
+++ react-app/src/components/Button.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  children: string;
+  color?: "primary" | "secondary" | "danger";
+  onClick: () => void;
+}
+
+const Button = ({ children, onClick, color = "primary" }: Props) => {
+  return (
+    <div className={"btn btn-" + color} onClick={onClick}>
+      {children}
+    </div>
+  );
+};
+
+export default Button;
