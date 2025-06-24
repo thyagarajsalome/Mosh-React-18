be78174bc260a6e720671be876691c1973fadaca
https://github.com/thyagarajsalome/KQ-V1.0.0.git <thyagarajsalome@gmail.com>
2025-06-24 11:35:56 +0530

    React 18 inisited

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,11 @@
+// PascalCasing
+
+function Message() {
+  // jsx: javascript
+
+  const name = "Mosh";
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello world</h1>;
+}
+
+export default Message;
---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+  // (item: string)=> void
+
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  const [selectedIndex, setselectedIndex] = useState(-1);
+
+  // Event handler
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setselectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
